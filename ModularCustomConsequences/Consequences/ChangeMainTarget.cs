9fe44f4f1ec36d61db0a42b08b8a788e72ea67db
ANSD_7 <ansddeveloper@gmail.com>
2026-05-03 13:25:54 +0700

    Added 'Count' argument for changemaintarget

--- ModularCustomConsequences/Consequences/ChangeMainTarget.cs
+++ ModularCustomConsequences/Consequences/ChangeMainTarget.cs
@@ -9,8 +9,10 @@ public class ConsequenceChangeMainTarget: IModularConsequence
 	public void ExecuteConsequence(ModularSA modular, string section, string circledSection, string[] circles)
 	{
 		BattleUnitModel attacker = modular.GetTargetModel(circles[0]);
-        int SkillID = modular.GetNumFromParamString(circles[1]);
-        BattleUnitModel target = modular.GetTargetModel(circles[2]);
+        BattleUnitModel target = modular.GetTargetModel(circles[1]);
+        int SkillID = modular.GetNumFromParamString(circles[2]);
+        int Count = 99;
+        if(circles.Length > 3) Count = modular.GetNumFromParamString(circles[3]);
 
         if (attacker == null || target == null) return;
 
@@ -18,13 +20,15 @@ public class ConsequenceChangeMainTarget: IModularConsequence
 
         foreach(SinActionModel sam in attacker.GetSinActionList())
         {
-            if (sam.CurrentBattleAction.Skill.GetID() == SkillID)
+            if (sam.CurrentBattleAction.Skill.GetID() == SkillID && Count > 0)
             {
                 SinActionModel targetSam = target.GetSinActionList()[0];
                 BattleActionModel attackerAction = sam.CurrentBattleAction;                
                 BattleActionModel targetAction = targetSam.CurrentBattleAction;
                 
                 attackerAction.ChangeMainTargetSinAction(targetSam, targetAction, true);
+
+                Count-=1;
 
                 // var bamManager = Singleton<BattleActionModelManager>.Instance;
                 // if (bamManager != null && targetAction != null)
