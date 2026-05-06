3d319bf3ac32528dc008c979e3115486b58241cb
Mellohi <mallohi138@gmail.com>
2026-05-06 11:18:58 +0300

    Add Acquirer isreusedskill

---
+++ ModularCustomConsequences/Acquirers/IsReusedSkill.cs
@@ -0,0 +1,18 @@
+﻿using ModularSkillScripts;
+
+namespace MTCustomScripts.Acquirers;
+
+public class AcquirerIsReusedSkill : IModularAcquirer
+{
+	public int ExecuteAcquirer(ModularSA modular, string section, string circledSection, string[] circles)
+	{
+		BattleActionModel action = modular.modsa_selfAction;
+		if (circles[0] == "Target") action = modular.modsa_oppoAction;
+
+		if (action.IsReusedAction())
+		{
+			return 1;
+		}
+		return 0;
+	}
+}
