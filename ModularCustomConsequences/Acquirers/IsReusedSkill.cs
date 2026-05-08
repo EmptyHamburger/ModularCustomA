56b221d732e996e0bbf05716d05c3496e92d2f9d
ANSD_7 <ansddeveloper@gmail.com>
2026-05-08 23:27:25 +0700

    make isreusedskill supports `MainTarget`

--- ModularCustomConsequences/Acquirers/IsReusedSkill.cs
+++ ModularCustomConsequences/Acquirers/IsReusedSkill.cs
@@ -7,7 +7,7 @@ public class AcquirerIsReusedSkill : IModularAcquirer
 	public int ExecuteAcquirer(ModularSA modular, string section, string circledSection, string[] circles)
 	{
 		BattleActionModel action = modular.modsa_selfAction;
-		if (circles[0] == "Target") action = modular.modsa_oppoAction;
+		if (circles[0] != "Self") action = modular.modsa_oppoAction;
 
 		if (action.IsReusedAction())
 		{
