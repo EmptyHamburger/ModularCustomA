ee7dda1ee0f83ae909cecfee242669e40349c251
ANSD_7 <ansddeveloper@gmail.com>
2026-06-13 10:43:01 +0700

    update version

--- ModularCustomConsequences/ModularCustomConsequencesMain.cs
+++ ModularCustomConsequences/ModularCustomConsequencesMain.cs
@@ -43,7 +43,7 @@ public class Main : BasePlugin
 {
     // Edit the below to your own plugin name, version, etc.
     public const string NAME = "MTCustomScripts";
-    public const string VERSION = "22.94.4";
+    public const string VERSION = "22.95.4";
     public const string AUTHOR = "MT";
     public const string GUID = $"{AUTHOR}.{NAME}";
 
