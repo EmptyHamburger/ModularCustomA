aedb52787cecc0163b5ee6ee0146dde37059c147
ANSD_7 <ansddeveloper@gmail.com>
2026-07-12 20:22:17 +0700

    Added 1 more arg for modifysubtarget, allowing the user to not consider targeted units when using modular's custom targeting

--- ModularCustomConsequences/Consequences/ModifySubTargetList.cs
+++ ModularCustomConsequences/Consequences/ModifySubTargetList.cs
@@ -1,5 +1,6 @@
 using ModularSkillScripts;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace MTCustomScripts.Consequences;
 
@@ -9,10 +10,43 @@ public class ConsequenceModifySubTargetList : IModularConsequence
     {
         if (modular.modsa_selfAction == null) return;
         string mode = circles[0];
-        Il2CppSystem.Collections.Generic.List<BattleUnitModel> targetList = modular.GetTargetModelList(circles[1]);
-        Il2CppSystem.Collections.Generic.List<BattleUnitModel> excludeList = modular.GetTargetModelList(circles[2]);
+        bool exceptTargetedUnits = modular.GetBoolFromParamString(circles[1]);
+        string inputParamTarget = circles[2];
+        Il2CppSystem.Collections.Generic.List<BattleUnitModel> targetList = new();
+        Il2CppSystem.Collections.Generic.List<BattleUnitModel> excludeList = modular.GetTargetModelList(circles[3]);
 
-        for (int i = excludeList.Count - 1; i > -1; i--)
+        if (exceptTargetedUnits)
+        {
+            string paramTarget = inputParamTarget;
+            string numText = Regex.Replace(inputParamTarget, "\\D", "");
+            int requestedTargetNum = 99;
+
+            if (numText.Length > 0)
+            {
+                requestedTargetNum = int.Parse(numText);
+                paramTarget = inputParamTarget.Replace(numText, "");
+            }
+
+            Il2CppSystem.Collections.Generic.List<BattleUnitModel> fullTargetList = modular.GetTargetModelList(paramTarget + "99");
+
+            HashSet<BattleUnitModel> alreadyTargeted = [];
+            Il2CppSystem.Collections.Generic.List<TargetSinActionData> currentSet = modular.modsa_selfAction._targetDataDetail.GetCurrentTargetSet()._subTargetList;
+            
+            if (modular.modsa_selfAction._targetDataDetail.GetMainTarget() != null) alreadyTargeted.Add(modular.modsa_selfAction._targetDataDetail.GetMainTarget());
+            foreach (TargetSinActionData TSAD in currentSet)
+            alreadyTargeted.Add(TSAD._targetSinAction._unitModel);
+
+            for (int i = 0; i < fullTargetList.Count; i++)
+            {
+                BattleUnitModel unit = fullTargetList[i];
+                if (alreadyTargeted.Contains(unit)) continue;
+                targetList.Add(unit);
+                if (targetList.Count >= requestedTargetNum) break;
+            }
+        }
+        else targetList = modular.GetTargetModelList(inputParamTarget);
+
+        for (int i = targetList.Count - 1; i > -1; i--)
         {
             if (excludeList.Contains(targetList[i])) targetList.RemoveAt(i);
         }
@@ -29,18 +63,19 @@ public class ConsequenceModifySubTargetList : IModularConsequence
         }
         else
         {
-            Il2CppSystem.Collections.Generic.List<TargetSinActionData> removeSubTargetList = new Il2CppSystem.Collections.Generic.List<TargetSinActionData>();
+            Il2CppSystem.Collections.Generic.List<TargetSinActionData> removeSubTargetList = new();
             
             foreach(BattleUnitModel target in targetList)
             {
                 foreach(TargetSinActionData TSAD in modular.modsa_selfAction._targetDataDetail.GetCurrentTargetSet()._subTargetList)
                 {
-                    if (TSAD._targetSinAction._unitModel.GetUnitID() == target.GetUnitID()) removeSubTargetList.Add(TSAD);
+                    if (TSAD._targetSinAction._unitModel == target) removeSubTargetList.Add(TSAD);
                 }
-                foreach(TargetSinActionData TSAD in removeSubTargetList)
-                {
-                    modular.modsa_selfAction._targetDataDetail.GetCurrentTargetSet()._subTargetList.Remove(TSAD);
-                }
+            }
+
+            foreach(TargetSinActionData TSAD in removeSubTargetList)
+            {
+                modular.modsa_selfAction._targetDataDetail.GetCurrentTargetSet()._subTargetList.Remove(TSAD);
             }
         }
     }
