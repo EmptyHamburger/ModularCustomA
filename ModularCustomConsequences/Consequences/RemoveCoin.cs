dedd9d49257db2a12f27fae42c24c44fead3bf02
ANSD_7 <ansddeveloper@gmail.com>
2026-08-16 15:03:06 +0700

    Fixed removecoin removing wrong coins

--- ModularCustomConsequences/Consequences/RemoveCoin.cs
+++ ModularCustomConsequences/Consequences/RemoveCoin.cs
@@ -9,7 +9,7 @@ public class ConsequenceRemoveCoin : IModularConsequence
 	public void ExecuteConsequence(ModularSA modular, string section, string circledSection, string[] circles)
 	{
 		SkillModel skill = (circles[0] == "Self") ? modular.modsa_skillModel : modular.modsa_oppoAction._skill;
-		foreach (string circle in circles.Skip(1))
+		foreach (string circle in circles.Skip(1).Reverse())
 		{
 			int idx = modular.GetNumFromParamString(circle);
 			if (idx < 0)
