438e6c193e0bdef42539e06b56e24442902febb1
ANSD_7 <ansddeveloper@gmail.com>
2026-09-12 13:10:11 +0700

    Revert setmaintarget changes

--- ModularCustomConsequences/Consequences/SetMainTarget.cs
+++ ModularCustomConsequences/Consequences/SetMainTarget.cs
@@ -22,14 +22,10 @@ public class ConsequenceSetMainTarget : IModularConsequence
 
             if (SkillID == -1)
             {
-                Singleton<BattleActionModelManager>.Instance.RemoveDuel(modular.modsa_selfAction);
                 SinActionModel selfSAM = modular.modsa_selfAction._sinAction;
                 SinActionModel targetSAM = target.GetSinActionList()[0];
                 modular.modsa_selfAction.ChangeMainTargetSinAction(targetSAM, targetSAM._currentBattleAction, true);
-                foreach(BattleActionModel bam in selfSAM.GetActionListTargetingThisSlot())
-                {
-                    selfSAM._actionSlot.SetActionTargetingThisSlot(bam);
-                }
+                // Singleton<SinManager>.Instance._targetManager.Refresh();
             }
             else
             {
@@ -43,12 +39,8 @@ public class ConsequenceSetMainTarget : IModularConsequence
                             BattleActionModel attackerAction = sam.CurrentBattleAction;                
                             BattleActionModel targetAction = targetSam.CurrentBattleAction;
                             
-                            Singleton<BattleActionModelManager>.Instance.RemoveDuel(attackerAction);
                             attackerAction.ChangeMainTargetSinAction(targetSam, targetAction, true);
-                            foreach(BattleActionModel bam in sam.GetActionListTargetingThisSlot())
-                            {
-                                sam._actionSlot.SetActionTargetingThisSlot(bam);
-                            }
+                            // Singleton<SinManager>.Instance._targetManager.Refresh();
 
                             Count-=1;
                         }
